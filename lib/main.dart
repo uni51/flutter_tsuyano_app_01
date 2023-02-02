dd60884327eb19dae11dfffd962240c022e3793a
uni51 <unieye51@gmail.com>
2023-02-02 09:06:23 +0900

    2-1　プロジェクトの作成

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Flutter'),
+        ),
+        body: Text(
+          'Hello Flutter World!',
+          style: TextStyle(fontSize: 32.0),
+        )
+      ),
+    );
+  }
+}
