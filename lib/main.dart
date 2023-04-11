e47e40b8a8ff301a634f683b2b396e2fd13a0bf1
uni51 <unieye51@gmail.com>
2023-04-11 19:36:46 +0900

    2-3 ウィジェットの基本レイアウト

--- lib/main.dart
+++ lib/main.dart
@@ -3,70 +3,48 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  final title = 'Flutterサンプル';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(
-          title: this.title,
-        ),
+      title: 'Generated App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({title: 'Flutter Demo'}) : super();
-
+  MyHomePage({Key? key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-// データ用クラス
-class Data {
-  String _name;
-  int _price;
-  Data(this._name, this._price): super();
-
-  @override
-  String toString() {
-    return _name + ':' + _price.toString() + '円';
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
-
-  static final _data = [
-    Data('Apple',200),
-    Data('Orange', 150),
-    Data('Peach', 300)
-  ];
-  Data _item = _data[0];
-
-  void _setData() {
-    setState(() {
-      _item = (_data..shuffle()).first; // カスケード記法（複数のメソッドをつなげて1行で処理する）
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Set data'),
+        title: Text('App Name'),
       ),
-      body: Text(
-        _item.toString(),
-        style: TextStyle(fontSize: 32.0),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _setData,
-        tooltip: 'set message.',
-        child: Icon(Icons.star),
+      body:
+      Container(
+        child:
+        Text(
+          "Hello Flutter!",
+          style: TextStyle(fontSize:32.0,
+              color: const Color(0xff000000),
+              fontWeight: FontWeight.w700,
+              fontFamily: "Roboto"),
+        ),
+        padding: const EdgeInsets.all(10.0),
+        alignment: Alignment.bottomCenter,
       ),
     );
   }
