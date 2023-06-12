6e8f841fb817cf4dbe9cdc14485b19a162578e8d
uni51 <unieye51@gmail.com>
2023-06-12 14:49:41 +0900

    3-2-6 DropdownButton

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static var _message = 'ok.';
-  static var _selected = 'A';
+  static var _selected = 'One';
 
   @override
   Widget build(BuildContext context) {
@@ -57,44 +57,22 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: EdgeInsets.all(10.0),
             ),
 
-            Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
+            DropdownButton<String>(
+              onChanged: popupSelected,
+              value: _selected,
+              style: const TextStyle(color:Colors.black,
+                  fontSize:28.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Roboto'),
 
-                  Radio<String>(
-                    value: 'A',
-                    groupValue: _selected,
-                    onChanged: checkChanged,
-                  ),
-                  const Text(
-                    "radio A",
-                    style: TextStyle(fontSize:28.0,
-                        fontWeight: FontWeight.w400,
-                        fontFamily: "Roboto"),
-                  )
-                ]
-            ),
-
-            Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-
-                  Radio<String>(
-                    value: 'B',
-                    groupValue: _selected,
-                    onChanged: checkChanged,
-                  ),
-                  const Text(
-                    "radio B",
-                    style: TextStyle(fontSize:28.0,
-                        fontWeight: FontWeight.w400,
-                        fontFamily: "Roboto"),
-                  )
-                ]
+              items: const <DropdownMenuItem<String>>[
+                DropdownMenuItem<String>(value: 'One',
+                    child: Text('One')),
+                DropdownMenuItem<String>(value: 'Two',
+                    child: Text('Two')),
+                DropdownMenuItem<String>(value: 'Three',
+                    child: Text('Three')),
+              ],
             ),
           ],
         ),
@@ -102,9 +80,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void checkChanged(String? value){
+  void popupSelected(String? value){
     setState(() {
-      _selected = value ?? 'nodata';
+      _selected = value ?? 'not selected...';
       _message = 'select: $_selected';
     });
   }
